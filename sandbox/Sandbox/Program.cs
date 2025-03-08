047cf82de7f52d7c5d29d448088acb665ed510e8
Anna Mosquera <anamariamosquera1023@gmail.com>
2025-03-07 21:06:44 -0500

    Added exclamation points

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("My name is Ana");
+        Console.WriteLine("Hello World!!!");
     }
 }
